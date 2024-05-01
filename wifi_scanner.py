723460b123f66661fbbbf0c0db3db11ce214f453
salmanmallah <mallahsalman06@gmail.com>
2024-05-01 20:17:42 +0500

    added file io

--- wifi_scanner.py
+++ wifi_scanner.py
@@ -2,7 +2,10 @@ import subprocess
 devices=subprocess.check_output(['netsh' , 'wlan' ,'show' ,'networks'])
 devices=devices.decode('ascii')
 devices=devices.replace('\r',"")
+
+# to write the the output in txt file:
+with open("result.txt", 'w') as wf:
+    wf.write(devices)
+
 print(devices)
-
-
 
